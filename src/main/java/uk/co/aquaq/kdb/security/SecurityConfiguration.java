bbdc739473be75174a1c372908b8f7350a029d75
katie-harrison-aquaq <Katie.Harrison@aquaq.co.uk>
2018-11-20 15:28:08 +0000

    Addition of configuration for LDAP authentication

--- src/main/java/uk/co/aquaq/kdb/security/SecurityConfiguration.java
+++ src/main/java/uk/co/aquaq/kdb/security/SecurityConfiguration.java
@@ -3,43 +3,81 @@ package uk.co.aquaq.kdb.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.*;
 
+import java.util.Base64;
 
 @Configuration
 @EnableWebSecurity
 @EnableWebMvc
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private AuthenticationEntryPoint authEntryPoint;
+    @Value("${security.ldap.url}")
+    private String url;
+    @Value("${managerDn}")
+    private String managerDn;
+    @Value("${managerPassword}")
+    private String managerPassword;
+    @Value("${groupSearchFilter}")
+    private String groupSearchFilter;
+    @Value("${userDnPatterns}")
+    private String userDnPatterns;
+    @Value("${userSearchBase}")
+    private String userSearchBase;
+    @Value("${userSearchFilter}")
+    private String userSearchFilter;
     @Value("${basic.authentication.user}")
-    String user;
+    private String basicAuthUsername;
     @Value("${basic.authentication.password}")
-    String password;
+    private String basicAuthPassword;
+    @Value("${authorisation.type}")
+    private String authType;
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser(user).password(password).authorities("ROLE_USER");
-    }
-
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin()
                 .and()
                 .httpBasic().and().cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
-
     }
 
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        switch(authType.trim().toUpperCase()) {
+            case "LDAP":
+                configureLdapAuth(auth);
+                break;
+            default:
+                configureBasicAuth(auth);
+                break;
+        }
+    }
 
+    private void configureBasicAuth(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(basicAuthUsername).password(basicAuthPassword).authorities("ROLE_USER");
+    }
+
+    private void configureLdapAuth(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .ldapAuthentication()
+                .userDnPatterns(userDnPatterns)
+                .userSearchFilter(userSearchFilter)
+                .userSearchBase("")
+                .groupSearchFilter(groupSearchFilter)
+                .contextSource()
+                .url(url)
+                .managerDn(managerDn)
+                .managerPassword(managerPassword);
+    }
 }
